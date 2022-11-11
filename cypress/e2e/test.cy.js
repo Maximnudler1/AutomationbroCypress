cac0e10c1053d7c6e1ee9e491d1ac74c78d0f1ca
Maximnudler1 <maximnudler@gmail.com>
2022-11-11 15:23:41 +0200

    final version

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,28 +1,21 @@
 /// <reference types= "cypress" />
 describe('My First suite', () => {
 
-
     beforeEach('open automationbro site',()=>{
         cy.openAutomationBro()
     })
 
-
-    // it('test register to automationbro',()=>{
-  
+    // it('test register to automationbro',()=>{  
     //     cy.get('#reg_username').type('testotorio')
     //     cy.get('#reg_email').type('test@otorio.co.il')
     //     cy.get('#reg_password').type('testotorio135!')
     //     cy.get('button[name="register"]').click()
     //     cy.get('.woocommerce-MyAccount-content p').should('contain.text','Hello testotorio')
-
     // })
-
 
     it('test sign in to automationbro', () =>{
         cy.signIn('testotorio','testotorio135!')
         cy.get('.woocommerce-MyAccount-content').should('contain.text','Hello testotorio')
-
-
     })
 
     it('Test incorrect email and password', () => {
@@ -31,6 +24,7 @@ describe('My First suite', () => {
             expect(message.text()).to.have.string('Unknown email address. Check again or try your username.')
         })
     })
+
     it('Test empty email and password', () => {
         cy.get('button[name="login"]').click()
         cy.get('.woocommerce-notices-wrapper li').then((message) => {
@@ -38,12 +32,10 @@ describe('My First suite', () => {
         })
     })
 
-
     it('Test Sign out and check it was successful.', () => {
         cy.signIn('testotorio','testotorio135!')
         cy.contains('Log out').click()
-        cy.url().should('include', 'https://practice.automationbro.com/my-account/')
-        
+        cy.url().should('include', 'https://practice.automationbro.com/my-account/')        
     })
 
     it('Test signin and add item it to cart', () => {
@@ -54,9 +46,5 @@ describe('My First suite', () => {
         cy.get('.tg-icon-shopping-cart:visible').click()
         cy.get('.product-name a').should('have.text','Branded Converse')
         cy.get('td[data-title="Total"]').should('have.text', '$150.00 ')
-        
     })
-
-
 })
-
